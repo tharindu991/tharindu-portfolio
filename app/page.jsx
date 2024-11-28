f9448c8c1f7660cc27f1a56917f40bc2e66e694c
Tharindu Bandara <tharinduudeni@gmail.com>
2024-11-28 21:52:55 +0000

    Home page implementation

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,43 @@
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+//components
+import Socials from "@/components/Socials";
+
 const Home = () => {
-  return <div>Homepage</div>;
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br />{" "}
+              <span className="text-accent">Tharindu Bandara</span>{" "}
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I exel at crafting elegant digital experience and I am proficient
+              in various programming language and technologies.
+            </p>
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Socials />
+              </div>
+            </div>
+          </div>
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -40,7 +40,7 @@ const MobileNav = (props) => {
       <SheetContent className="flex flex-col">
         <div className="mt-32 mb-40 text-center text-2xl">
           <Link href="/">
-            <h1 className="text-4xl font-semibold">Luke</h1>
+            <h1 className="text-4xl font-semibold"></h1>
           </Link>
         </div>
         <nav className="flex flex-col justify-center items-center gap-8">
