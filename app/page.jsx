04cc2a0d82989a12052c5a4cf760411bc37fe186
Tharindu Bandara <tharinduudeni@gmail.com>
2024-12-01 11:25:19 +0000

    home page enhancements

--- app/page.jsx
+++ app/page.jsx
@@ -3,13 +3,14 @@ import { FiDownload } from "react-icons/fi";
 
 //components
 import Socials from "@/components/Socials";
+import Photo from "@/components/Photo";
 
 const Home = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
               Hello I'm <br />{" "}
@@ -29,11 +30,16 @@ const Home = () => {
                 <FiDownload className="text-xl" />
               </Button>
               <div className="mb-8 xl:mb-0">
-                <Socials />
+                <Socials
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
               </div>
             </div>
           </div>
-          <div>photo</div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
       </div>
     </section>
