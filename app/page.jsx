338a93229e6dd667a9422c0c518aeb9148543a6e
Tharindu Bandara <tharinduudeni@gmail.com>
2024-12-07 22:19:06 +0000

    home page animations

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import { FiDownload } from "react-icons/fi";
 //components
 import Socials from "@/components/Socials";
 import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
 
 const Home = () => {
   return (
@@ -42,6 +43,7 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 };
