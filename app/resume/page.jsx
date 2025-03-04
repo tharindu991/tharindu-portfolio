5334f82160d966ef4a8505df32aae8f52f14bf61
Tharindu Bandara <tharinduudeni@gmail.com>
2025-03-04 19:42:56 +0000

    Resume page ui implementation

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -21,6 +21,7 @@ import {
 } from "@/components/ui/tooltip";
 
 import { motion } from "framer-motion";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
 
 const about = {
   title: "About me",
@@ -176,7 +177,40 @@ const Resume = () => {
 
           <div className="min-h-[70vh] w-full">
             <TabsContent value="experience" className="w-full">
-              experience
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {experience.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {experience.items.map((item, index) => {
+                      return (
+                        <li
+                          className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg-items-start gap-1"
+                          key={index}
+                        >
+                          <span>{item.duration}</span>
+                          <h3>{item.position}</h3>
+                          <div>
+                            <span></span>
+                            <p>{item.company}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="education" className="w-full">
+              Education
+            </TabsContent>
+            <TabsContent value="skills" className="w-full">
+              Skills
+            </TabsContent>
+            <TabsContent value="about" className="w-full">
+              About Me
             </TabsContent>
           </div>
         </Tabs>
