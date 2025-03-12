a911cadbdb86bef3f7f40f569e372770e9363ffc
Tharindu Bandara <tharinduudeni@gmail.com>
2025-03-12 20:27:49 +0000

    fix: ui fixes

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -163,7 +163,7 @@ const Resume = () => {
       initial={{ opacity: 0 }}
       animate={{
         opacity: 1,
-        transition: { delay: 2.4, duration: 0.4, ease: "eastIn" },
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
       }}
       className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
     >
