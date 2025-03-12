435f7f67c6d96f951fe7568c5e19942e44eb4610
Tharindu Bandara <tharinduudeni@gmail.com>
2025-03-12 20:28:30 +0000

    feat: work page slider implementation

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -15,6 +15,7 @@ import {
 
 import Link from "next/link";
 import Image from "next/image";
+import WorkSliderBtns from "@/components/WorkSliderBtns";
 
 const projects = [
   {
@@ -133,8 +134,8 @@ const Work = () => {
                 return (
                   <SwiperSlide key={index} className="w-full">
                     <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
-                      <div></div>
-                      <div>
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                      <div className="relative w-full h-full">
                         <Image
                           src={project.image}
                           fill
@@ -146,6 +147,12 @@ const Work = () => {
                   </SwiperSlide>
                 );
               })}
+              <WorkSliderBtns
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                btnStyles={
+                  "bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+                }
+              />
             </Swiper>
           </div>
         </div>
