f38a7409b04636ea0f57102dea54061672c90dda
Tharindu Bandara <tharinduudeni@gmail.com>
2025-04-10 13:20:52 +0100

    feat: update service descriptions for improved clarity and detail

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -9,28 +9,28 @@ const services = [
     num: "01",
     title: "Web Development",
     description:
-      "Building scalable and high-performance web applications using React.js, TypeScript, Redux, MobX and AWS serverless technologies. Experienced in SEO-friendly, responsive design and Progressive Web Apps (PWAs).",
+      "Building scalable and high-performance web applications using React.js, Next.js, Wordpress, TypeScript, Redux, MobX and AWS serverless technologies. Experienced in SEO-friendly, responsive design and Progressive Web Apps (PWAs).",
     href: "",
   },
   {
     num: "02",
     title: "Mobile Development",
     description:
-      "Expert in React Native and Android development, crafting seamless cross-platform and native mobile applications. Integrated deep linking, authentication, and cloud services for enhanced user experiences.",
+      "Expert in React Native (Android, iOS) and native Android development (Java, Kotlin), crafting seamless cross-platform and native mobile applications. Integrated deep linking, authentication, and cloud services for enhanced user experiences.",
     href: "",
   },
   {
     num: "03",
     title: "UI/UX Design",
     description:
-      "Creating intuitive and user-friendly interfaces with Atomic Design principles, Storybook, Tailwind CSS, and WCAG-compliant accessibility standards. Collaborating closely with designers to enhance UX.",
+      "Creating intuitive and user-friendly interfaces with HTML5, CSS3, Tailwind CSS, Storybook, Atomic Design principles and WCAG-compliant accessibility standards. Collaborating closely with designers to enhance UX.",
     href: "",
   },
   {
     num: "04",
     title: "Cloud & DevOps",
     description:
-      "Deploying and managing cloud-based applications with AWS Lambda, Amplify, and CI/CD pipelines. Experience in automating deployments and optimizing cloud infrastructure for scalability and reliability.",
+      "Deploying and managing cloud-based applications with AWS Lambda, Amplify, and CI/CD pipelines. Experience in automating deployments and optimising cloud infrastructure for scalability and reliability.",
     href: "",
   },
   {
